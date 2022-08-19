a88da04238aec7920dc138863cc7ec339db92291
TA Shreeshylesh <68643579+Sri-Shylesh@users.noreply.github.com>
2022-08-19 16:48:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "[server]\n
+echo "
+[general]\n
+email = "shylesh201@gmail.com"\n
+" > ~/.streamlit/credentials.toml
+echo "
+[server]\n
 headless = true\n
 enableCORS=false\n
 port = $PORT\n
-\n
 " > ~/.streamlit/config.toml
